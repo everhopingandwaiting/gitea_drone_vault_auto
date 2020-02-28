f409d1ff326268002b38a116dce0e25ff8a010a6
root <root@localhost.localdomain>
2020-02-28 23:42:35 +0800

    scale runner

--- start.sh
+++ start.sh
@@ -69,10 +69,10 @@ systemctl start docker.service
 echo "#######################start##########################"
 docker-compose config
 echo "########################end#########################"
-#  docker-compose pull --include-deps
+ docker-compose pull --include-deps
 # go go go ko
 docker-compose up --force-recreate  --remove-orphans -d
-# docker-compose up  --remove-orphans -d
+docker-compose up  --remove-orphans -d --scale ssh-runner=2 docker-runner=2
 docker-compose logs -t --tail="1000"
 # or 
 # docker stack deploy -c docker-compose.yml gitea_all
